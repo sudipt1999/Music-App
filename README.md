c1114f2eba43081156979e88023ca2dc5ad4ef9d
sudipt1999 <sudiptdabral2991999@gmail.com>
2020-08-09 00:16:03 +0530

    working completed

---
+++ README.md
@@ -0,0 +1,9 @@
+# Music App
+A music app that uses Youtube API for fetching music and playing it in browser.
+
+## Run it locally 
+1. Clone the repo
+2. Do a `npm install`
+3. Put youtube API keys in the sample env file and rename it to `.env`
+4. Run it using `npm start`
+
---
+++ src/components/SwipeMenu.js
@@ -0,0 +1,95 @@
+import React, { useState, useContext, useEffect, useCallback } from "react";
+
+import {
+  SwipeableDrawer,
+  Divider,
+  Link as MaterialLink
+} from "@material-ui/core";
+
+
+
+import { motion, AnimatePresence } from "framer-motion";
+
+import { Link } from "react-router-dom";
+
+import { GlobalContext } from "./GlobalState";
+import "./darkMode.css";
+import moon from "../images/moon-solid.svg";
+import sun from "../images/sun-solid.svg";
+
+const SwipeMenu = () => {
+  const [{ menuOpen, themeSelectValue }, dispatch] = useContext(GlobalContext);
+
+  const setMenuOpen = data => {
+    dispatch({ type: "setMenuOpen", snippet: data });
+  };
+
+  const setThemeSelectValue = useCallback(
+    data => {
+      dispatch({ type: "setThemeSelectValue", snippet: data });
+    },
+    [dispatch]
+  );
+
+  const [isNight, setIsNight] = useState(false);
+
+  useEffect(() => {
+    if (themeSelectValue === "Dark") {
+      setIsNight(true);
+    } else {
+      setIsNight(false);
+    }
+  }, [themeSelectValue]);
+
+  const changeTheme = theme => {
+    setThemeSelectValue(theme);
+    localStorage.setItem("selectedTheme", theme);
+  };
+
+  const handleThemeToggle = () => {
+    if (!isNight) {
+      changeTheme("Dark");
+      setIsNight(false);
+    } else {
+      changeTheme("Default");
+      setIsNight(true);
+    }
+  };
+
+  return (
+    <SwipeableDrawer
+      open={menuOpen}
+      onClose={() => setMenuOpen(false)}
+      onOpen={() => setMenuOpen(true)}
+    >
+      <div style={{ width: "300px" }}>
+        <div
+          style={{
+            margin: "35px",
+            position: "relative",
+            width: "30px",
+            height: "30px"
+          }}
+        >
+          <AnimatePresence>
+            <motion.img
+              key={isNight ? sun : moon}
+              initial={{ scale: 0 }}
+              animate={{ scale: 1.5, rotate: "360deg" }}
+              exit={{ scale: 0 }}
+              // transition={{ duration: 0.5 }}
+              src={isNight ? sun : moon}
+              onClick={() => handleThemeToggle()}
+              className="dayNightToggleBtn"
+              alt="sun moon icon"
+            />
+          </AnimatePresence>
+        </div>
+
+        <Divider />
+       </div>
+    </SwipeableDrawer>
+  );
+};
+
+export default SwipeMenu;
